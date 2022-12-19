94fd094183ca5aaaa56525dcb4f082b1ee8ca6ca
roshana01 <roshanaroshani99@gmail.com>
2022-12-19 12:52:37 +0330

    add sidebar styles

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -3,29 +3,28 @@ import './SideBar.css'
 export default function SideBar() {
   return (
     <div className="sideBar">
-      <h1 className="sideBar-title">به داشبورد خوش امدید!</h1>
+      <h1 className="sideBar-title">به داشبورد خوش آمدید!</h1>
 
       <ul className="sideBar-links">
         <li>
           <a href="#">صفحه اصلی</a>
         </li>
-        <li>
+        <li className='active'>
           <a href="#">محصولات</a>
+        </li>
+
+        <li>
+          <a href="#">سفارشات</a>
         </li>
         <li>
           <a href="#">کامنت ها</a>
         </li>
         <li>
-          <a href="#">کابران</a>
-        </li>
-        <li>
-          <a href="#">سفارشات</a>
-        </li>
-        <li>
-          <a href="#">صفحه اصلی</a>
-        </li>
-        <li>
           <a href="#">تخفیفات</a>
+        </li>
+
+        <li>
+          <a href="#">کابران</a>
         </li>
       </ul>
     </div>
