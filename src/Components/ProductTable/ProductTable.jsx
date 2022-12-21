1f324c30e59bd3d3429181940dcde182e9090fcd
roshana01 <roshanaroshani99@gmail.com>
2022-12-21 12:14:52 +0330

    add delete modal template

--- src/Components/ProductTable/ProductTable.jsx
+++ src/Components/ProductTable/ProductTable.jsx
@@ -27,30 +27,34 @@ export default function ProductTable() {
                             <button className="product-table-btn">ویرایش</button>
                         </td>
                     </tr>
+                    <tr>
+                        <td scope="row">
+                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
+                        </td>
+                        <td>گوشی ایفون 13 پرو مکس</td>
+                        <td>55.000.000</td>
+                        <td>85</td>
+                        <td className='product-table-btn-tr'>
+                            <button className="product-table-btn">جزییات</button>
+                            <button className="product-table-btn">حذف</button>
+                            <button className="product-table-btn">ویرایش</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td scope="row">
+                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
+                        </td>
+                        <td>گوشی ایفون 13 پرو مکس</td>
+                        <td>55.000.000</td>
+                        <td>85</td>
+                        <td>
+                            <button className="product-table-btn">جزییات</button>
+                            <button className="product-table-btn">حذف</button>
+                            <button className="product-table-btn">ویرایش</button>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
-            {/* <table className='products-table'>
-            <tr className="products-table-heading-tr">
-                <th>عکس</th>
-                <th>اسم</th>
-                <th>قیمت</th>
-                <th>موجودی</th>
-
-            </tr>
-            <tr className='products-table-tr'>
-                <td>
-                    <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
-                </td>
-                <td>گوشی ایفون 13 پرو مکس</td>
-                <td>55.000.000</td>
-                <td>85</td>
-                <td>
-                    <button className="product-table-btn">جزییات</button>
-                    <button className="product-table-btn">حذف</button>
-                    <button className="product-table-btn"></button>
-                </td>
-            </tr>
-    </table> */}
         </div>
     )
 }
--- src/page/Comments/Comments.jsx
+++ src/page/Comments/Comments.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import DeletModal from '../../Components/DeletModal/DeletModal'
 import Error from '../../Components/Error/Error'
+
 export default function Comments() {
   return (
     <div>
-    <Error msg={'هیچ کامنتی یافت نشد...'}/>
-  </div>
+      <Error msg={'هیچ کامنتی یافت نشد...'} />
+      <DeletModal />
+    </div>
   )
 }
