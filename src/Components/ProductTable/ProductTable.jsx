55a1b1642960598d10f446f02b4fe5114fe989dd
roshana01 <roshanaroshani99@gmail.com>
2022-12-21 10:02:38 +0330

    add styles table

--- src/Components/ProductTable/ProductTable.jsx
+++ src/Components/ProductTable/ProductTable.jsx
@@ -3,7 +3,33 @@ import './ProductTable.css'
 
 export default function ProductTable() {
     return (
-        <table className='products-table'>
+        <div className="products-table">
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th scope='col'>عکس</th>
+                        <th scope='col'>اسم</th>
+                        <th scope='col'>قیمت</th>
+                        <th scope='col'>موجودی</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td scope="row">
+                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
+                        </td>
+                        <td>گوشی ایفون 13 پرو مکس</td>
+                        <td>55.000.000</td>
+                        <td>85</td>
+                        <td>
+                            <button className="product-table-btn">جزییات</button>
+                            <button className="product-table-btn">حذف</button>
+                            <button className="product-table-btn">ویرایش</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            {/* <table className='products-table'>
             <tr className="products-table-heading-tr">
                 <th>عکس</th>
                 <th>اسم</th>
@@ -11,7 +37,7 @@ export default function ProductTable() {
                 <th>موجودی</th>
 
             </tr>
-            <tr>
+            <tr className='products-table-tr'>
                 <td>
                     <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
                 </td>
@@ -21,9 +47,10 @@ export default function ProductTable() {
                 <td>
                     <button className="product-table-btn">جزییات</button>
                     <button className="product-table-btn">حذف</button>
-                    <button className="product-table-btn">ویرایش</button>
+                    <button className="product-table-btn"></button>
                 </td>
             </tr>
-        </table>
+    </table> */}
+        </div>
     )
 }
