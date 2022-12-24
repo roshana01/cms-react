ac5cf00fdf36cefc36a132cb636fa1b49e02e8f0
roshana01 <roshanaroshani99@gmail.com>
2022-12-24 10:01:36 +0330

    make delete modal dynamic

--- src/Components/DeleteModal/DeleteModal.jsx
+++ src/Components/DeleteModal/DeleteModal.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom'
 import './DeleteModal.css'
 
 
-export default function DeleteModal() {
+export default function DeleteModal({submitModal, canselModal}) {
     return ReactDOM.createPortal(
-        <div className='modal-parent'>
+        <div className='modal-parent active'>
             <div className="delete-modal">
                 <h1>آیا از حذف اطمینان دارید؟</h1>
                 <div className='delete-modal-btns'>
-                    <button className='delete-btn delete-modal-accept-btn'>بله</button>
-                    <button className='delete-btn delete-modal-reject-btn'>خیر</button>
+                    <button className='delete-btn delete-modal-accept-btn' onClick={ submitModal}>بله</button>
+                    <button className='delete-btn delete-modal-reject-btn' onClick={canselModal}>خیر</button>
                 </div>
             </div>
         </div>
--- src/Components/ProductTable/ProductTable.jsx
+++ src/Components/ProductTable/ProductTable.jsx
@@ -1,60 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import DeleteModal from '../DeleteModal/DeleteModal'
 import './ProductTable.css'
-
 export default function ProductTable() {
+    const [isShowModal, setIsShowModal] = useState(false)
+
+
+
+
+    const cancelModalHandler = () => {
+        console.log('مدال بسته شد');
+        setIsShowModal(false)
+        
+    }
+    const submitModalHandler = () => {
+        console.log('مدال تایید شد');
+        setIsShowModal(false)
+        
+    }
+
+
+
     return (
-        <div className="products-table">
-            <table className="table">
-                <thead className="thead-dark">
-                    <tr>
-                        <th scope='col'>عکس</th>
-                        <th scope='col'>اسم</th>
-                        <th scope='col'>قیمت</th>
-                        <th scope='col'>موجودی</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td scope="row">
-                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
-                        </td>
-                        <td>گوشی ایفون 13 پرو مکس</td>
-                        <td>55.000.000</td>
-                        <td>85</td>
-                        <td>
-                            <button className="product-table-btn">جزییات</button>
-                            <button className="product-table-btn">حذف</button>
-                            <button className="product-table-btn">ویرایش</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td scope="row">
-                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
-                        </td>
-                        <td>گوشی ایفون 13 پرو مکس</td>
-                        <td>55.000.000</td>
-                        <td>85</td>
-                        <td className='product-table-btn-tr'>
-                            <button className="product-table-btn">جزییات</button>
-                            <button className="product-table-btn">حذف</button>
-                            <button className="product-table-btn">ویرایش</button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td scope="row">
-                            <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
-                        </td>
-                        <td>گوشی ایفون 13 پرو مکس</td>
-                        <td>55.000.000</td>
-                        <td>85</td>
-                        <td>
-                            <button className="product-table-btn">جزییات</button>
-                            <button className="product-table-btn">حذف</button>
-                            <button className="product-table-btn">ویرایش</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <>
+            <div className="products-table">
+                <table className="table">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th scope='col'>عکس</th>
+                            <th scope='col'>اسم</th>
+                            <th scope='col'>قیمت</th>
+                            <th scope='col'>موجودی</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td scope="row">
+                                <img src="/img/goshi3.jpg" alt="" className='product-table-img' />
+                            </td>
+                            <td>گوشی ایفون 13 پرو مکس</td>
+                            <td>55.000.000</td>
+                            <td>85</td>
+                            <td>
+                                <button className="product-table-btn" >جزییات</button>
+                                <button className="product-table-btn" onClick={() => setIsShowModal(true)}>حذف</button>
+                                <button className="product-table-btn">ویرایش</button>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+            {/* console.log( {isShowModal && <DeleteModal />});  */}
+            {isShowModal && <DeleteModal submitModal={submitModalHandler} canselModal={cancelModalHandler}/>}
+        </>
     )
 }
