1c79b5c0012f4e59986f45f36ccbc4de0a7e95c7
roshana01 <roshanaroshani99@gmail.com>
2022-12-29 20:21:52 +0330

    feath comments data

--- src/page/Comments/Comments.jsx
+++ src/page/Comments/Comments.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DeleteModal from '../../Components/DeleteModal/DeleteModal'
 import Error from '../../Components/Error/Error'
-
+import './Comments.css'
 export default function Comments() {
+
+  const [allComments, setAllComments] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/comments/")
+      .then(res => res.json())
+      .then(commnt => setAllComments(commnt))
+  }, []);
+
+
   return (
-    <div>
-      <Error msg={'هیچ کامنتی یافت نشد...'} />
-    </div>
+    <>
+
+
+      <div className="comments-table">
+        {
+          allComments.length ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>نام کاربر</th>
+                  <th>محصول</th>
+                  <th> کامنت</th>
+                  <th>تاریخ</th>
+                  <th>ساعت</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                {
+                  allComments.map((item) => (
+
+                    <tr>
+                      <td>{item.userID}</td>
+                      <td>{item.productID}</td>
+                      <td><button className='btn-comments'>دیدن کامنت</button></td>
+                      <td>{item.date}</td>
+                      <td>{item.hour}</td>
+                      <td>
+                        <button className='btn-comments'>حذف</button>
+                        <button className='btn-comments'>ویرایش</button>
+                        <button className='btn-comments'>پاسخ</button>
+                        <button className='btn-comments'>تایید</button></td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          ) : (
+
+            <Error msg={'کامنتی یافت نشد!'} />
+          )
+        }
+      </div>
+
+    </>
   )
 }
